b5651d46bf741cb5bbb2684acd97f1acdabbe49c
unknown <nikhil98patil@gmail.com>
2022-03-11 00:26:59 +0530

    Added: Use Case to handle exception if user provides invalid mood

---
+++ UC2_InvalidMood/MoodAnalyzer.java
@@ -0,0 +1,29 @@
+package com.bridgelaz.UC2;
+
+public class MoodAnalyzer {
+	String message;
+
+	public MoodAnalyzer() {
+
+	}
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyzeMood() {
+
+		try {
+			if (this.message.contains("Sad")) {
+				return "SAD";
+			} else {
+				return "HAPPY";
+			}
+		} catch (NullPointerException e) {
+			return "HAPPY";
+		}
+	}
+}
+
+
+
