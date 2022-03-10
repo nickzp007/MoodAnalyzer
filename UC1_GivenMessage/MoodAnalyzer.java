f57296a1ddfd0fd349b9db3e5a738af5cf483ade
unknown <nikhil98patil@gmail.com>
2022-03-11 00:23:51 +0530

    Added: Use Case for Given message

---
+++ UC1_GivenMessage/MoodAnalyzer.java
@@ -0,0 +1,27 @@
+package com.bridgelaz;
+
+public class MoodAnalyzer {
+
+
+	String message;
+
+	public MoodAnalyzer() {
+
+	}
+
+	public MoodAnalyzer(String message) {
+		this.message = message;
+	}
+
+	public String analyzeMood () {
+
+		if (this.message.contains("Sad")) {
+			return "SAD";
+		} else {
+			return "HAPPY";
+		}
+
+	}
+}
+
+
